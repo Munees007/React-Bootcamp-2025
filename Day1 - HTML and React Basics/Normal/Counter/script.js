f871050e41f45e9bb49beda59386bec8992c9a2f
Munees007 <muneesm458@gmail.com>
2025-11-14 15:40:14 +0530

    super

--- Day1 - HTML and React Basics/Normal/Counter/script.js
+++ Day1 - HTML and React Basics/Normal/Counter/script.js
@@ -1,10 +1,13 @@
-let counter
-document.addEventListener("DOMContentLoaded", function ready() {
-  counter = document.getElementById("counter");
-  console.log("hello")
-});
-function Count() {
-    console.log(counter.innerHTML);
-    counter.innerText = parseInt(counter.innerText) + 1;
-}
+
+document.addEventListener("DOMContentLoaded",function ready(){
+ const  counter = document.getElementById("counter")
+  const button = document.getElementById("btn")
+
+
+  button.addEventListener("click",function Count()
+{
+  counter.innerText = parseInt(counter.innerText) + 1
+})
+
+})
 
